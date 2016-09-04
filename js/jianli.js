6bac6af58cd6ca0349ec06f72134301f01ef64e7
jvziking <1689849929@qq.com>
2016-09-04 16:13:39 +0800

    resume

---
+++ js/jianli.js
@@ -0,0 +1,17 @@
+$(function(){
+	$('#down1').click(function(event) {
+		$('#docu2').css('display','block').siblings('div').css('display','none')
+	});
+	$('#down2').click(function(event) {
+		$('#docu3').css('display','block').siblings('div').css('display','none')
+	});
+	$('#down3').click(function(event) {
+		$('#docu4').css('display','block').siblings('div').css('display','none')
+	});
+	$('#down4').click(function(event) {
+		$('#docu5').css('display','block').siblings('div').css('display','none')
+	});
+	$('#down5').click(function(event) {
+		$('#docu').css('display','block').siblings('div').css('display','none')
+	});
+})
